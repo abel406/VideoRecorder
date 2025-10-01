49bf569a0d43f6da5f034c7009bd5467ccb2349b
Abel Rodríguez Vera <abel.rodriguez@greathobbies.com>
2025-10-01 13:39:29 -0400

    Changing extensions

--- videorecorder.client/src/db/mediaDb.js
+++
@@ -1,6 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('videoRecorderDB');
-db.version(1).stores({
-    recordings: '++id, createdAt' // auto-increment id, index by createdAt
-});
